a65d7b6ca2c61eabe1f3793b19b7f040f6db4c66
pleuvoir <pleuvior@foxmail.com>
2018-09-03 00:03:23 +0800

    :blush: update GithubCommitAsyncThread

--- README.md
+++ README.md
@@ -20,13 +20,13 @@ github:
       targetRepository: 待提交仓库  
 ```
 
-打开 shell 或cmd，进入 ngrok 目录，运行 `ngrok -config ngrok.cfg -subdomain wechatmp-github-exchange 9000` 如果运行失败，请更换 wechatmp-github-exchange 为其它字符串，直至连接成功
+打开 shell 或cmd，进入 ngrok 目录，运行 `ngrok -config ngrok.cfg -subdomain wechatmp-github-exchange 9000` 如果运行失败，请更换 `wechatmp-github-exchange` 为其它字符串，直至连接成功
 
 
 ### 打包运行
 
 ```
-clean package
+mvn clean package
 java -jar wechatmp-github-exchange-1.0.0.jar
 ```
 
@@ -35,4 +35,4 @@ java -jar wechatmp-github-exchange-1.0.0.jar
 
 在 eclipse 中如果 Installed JREs 选择的是 jre 则在 eclipse 中连接 github 没有问题，但是却无法打包，如果改为了 jdk，则可以打包，却无法在 eclipse 中连接 github 
 
-解决方案为更新jdk目录下安全相关的包，可根据出现的错误具体解决
+解决方案为更新 jdk 目录下安全相关的包，可根据出现的错误具体解决
--- src/main/java/io/github/pleuvoir/wechatmp/MsgHandler.java
+++ src/main/java/io/github/pleuvoir/wechatmp/MsgHandler.java
@@ -1,13 +1,11 @@
 package io.github.pleuvoir.wechatmp;
 
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import com.alibaba.fastjson.JSON;
-
 import io.github.pleuvoir.github.GithubCommitService;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.session.WxSessionManager;
 import me.chanjar.weixin.mp.api.WxMpMessageHandler;
 import me.chanjar.weixin.mp.api.WxMpService;
@@ -15,7 +13,13 @@ import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutTextMessage;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import com.alibaba.fastjson.JSON;
+
+
+@Slf4j
 @Component
 public class MsgHandler implements WxMpMessageHandler{
 	
@@ -29,11 +33,43 @@ public class MsgHandler implements WxMpMessageHandler{
                                     WxSessionManager sessionManager) {
     	
         // 提交到摘录项目
-        githubCommitService.commit(wxMessage.getContent());
+		new GithubCommitAsyncThread(githubCommitService, wxMessage.getContent());
+        
         // 组装回复消息
         String content = "收到信息内容：" + JSON.toJSONString(wxMessage);
         return new TextBuilder().build(content, wxMessage, weixinService);
     }
+    
+    
+    
+	/**
+	 *  github 提交异步处理线程
+	 * 	<p> github 提交后页面显示内容已变，但实际上程序必须去另外的 url 下载文件内容，而这个是有延迟的，所以所有提交排队等待 5 分钟 后再去更新
+	 */
+	private static class GithubCommitAsyncThread extends Thread {
+		private GithubCommitService githubCommitService;
+		private String content;
+
+		public GithubCommitAsyncThread(GithubCommitService githubCommitService,
+				String content) {
+			this.githubCommitService = githubCommitService;
+			this.content = content;
+			start();
+		}
+
+		@Override
+		public void run() {
+			try {
+				synchronized (githubCommitService) {
+					TimeUnit.MINUTES.sleep(5);
+					githubCommitService.commit(content);
+				}
+			} catch (InterruptedException e) {
+				log.error("{} error，{}", this.getName(), e);
+			}
+		}
+	}
+    
     
     
     public class TextBuilder  {
