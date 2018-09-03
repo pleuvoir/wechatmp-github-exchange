5b07a4afae06b1d7c9a7d8acab9ec3450aecbbbf
pleuvoir <mailto:pleuvior@foxmail.com>
2018-09-03 15:35:56 +0800

    :boom: 提交线程改为公平锁

--- src/main/java/io/github/pleuvoir/wechatmp/MsgHandler.java
+++ src/main/java/io/github/pleuvoir/wechatmp/MsgHandler.java
@@ -1,10 +1,15 @@
 package io.github.pleuvoir.wechatmp;
 
-import io.github.pleuvoir.github.GithubCommitService;
-
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.alibaba.fastjson.JSON;
+
+import io.github.pleuvoir.github.GithubCommitService;
 import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.session.WxSessionManager;
 import me.chanjar.weixin.mp.api.WxMpMessageHandler;
@@ -12,11 +17,6 @@ import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutTextMessage;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import com.alibaba.fastjson.JSON;
 
 
 @Slf4j
@@ -32,9 +32,14 @@ public class MsgHandler implements WxMpMessageHandler{
                                     Map<String, Object> context, WxMpService weixinService,
                                     WxSessionManager sessionManager) {
     	
-        // 提交到摘录项目
-		new GithubCommitAsyncThread(githubCommitService, wxMessage.getContent());
         
+		try {
+			// 提交到摘录项目
+			new GithubCommitAsyncThread(githubCommitService, wxMessage.getContent());
+			// 休息一毫秒，保证并发时线程的顺序
+			TimeUnit.MILLISECONDS.sleep(1);
+		} catch (InterruptedException ignored) {}
+		
         // 组装回复消息
         String content = "收到信息内容：" + JSON.toJSONString(wxMessage);
         return new TextBuilder().build(content, wxMessage, weixinService);
@@ -47,11 +52,11 @@ public class MsgHandler implements WxMpMessageHandler{
 	 * 	<p> github 提交后页面显示内容已变，但实际上程序必须去另外的 url 下载文件内容，而这个是有延迟的，所以所有提交排队等待 5 分钟 后再去更新
 	 */
 	private static class GithubCommitAsyncThread extends Thread {
+		private static final ReentrantLock fairlock = new ReentrantLock(true);	
 		private GithubCommitService githubCommitService;
 		private String content;
 
-		public GithubCommitAsyncThread(GithubCommitService githubCommitService,
-				String content) {
+		public GithubCommitAsyncThread(GithubCommitService githubCommitService, String content) {
 			this.githubCommitService = githubCommitService;
 			this.content = content;
 			start();
@@ -60,16 +65,16 @@ public class MsgHandler implements WxMpMessageHandler{
 		@Override
 		public void run() {
 			try {
-				synchronized (githubCommitService) {
-					TimeUnit.MINUTES.sleep(5);
-					githubCommitService.commit(content);
-				}
+				fairlock.lock();
+				TimeUnit.MINUTES.sleep(5);
+				githubCommitService.commit(content);
 			} catch (InterruptedException e) {
 				log.error("{} error，{}", this.getName(), e);
+			} finally {
+				fairlock.unlock();
 			}
 		}
 	}
-    
     
     
     public class TextBuilder  {
